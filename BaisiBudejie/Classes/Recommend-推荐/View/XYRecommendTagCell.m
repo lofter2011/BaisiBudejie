fd7e7aa3de6df9bdb24b26ec72cf26fb746f07bc
icebacky <backyer@qq.com>
2015-12-08 19:51:22 +0800

    推荐标签Debug
    修正了推荐标签页面点击图片会放大错位的问题

--- BaisiBudejie/Classes/Recommend-推荐/View/XYRecommendTagCell.m
+++ BaisiBudejie/Classes/Recommend-推荐/View/XYRecommendTagCell.m
@@ -38,10 +38,10 @@
     
     // 设置图片(圆形图片)
     UIImage *placeholder = [UIImage xy_circleImageNamed:@"defaultUserIcon"];
-    [self.imageView sd_setImageWithURL:[NSURL URLWithString:recommendTag.image_list] placeholderImage:placeholder completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
+    [self.imageListView sd_setImageWithURL:[NSURL URLWithString:recommendTag.image_list] placeholderImage:placeholder completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
         if (!image) return;
         // 将图片处理成圆形
-        self.imageView.image = [image xy_circleImage];
+        self.imageListView.image = [image xy_circleImage];
     }];
     
     self.themeNameLabel.text = recommendTag.theme_name;
